71554e8fe251d4e9c94bd5bef4533889938d6d34
David Wilson <davidwil@posteo.de>
2021-05-05 20:51:57 +0100

    Pressing enter on base url now saves it.

--- settings.js
+++ settings.js
@@ -7,16 +7,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const goHome = () => location.href = 'issue.html';
 
+  const saveClicked = e => {
+    e.preventDefault();
+    chrome.storage.sync.set({'base_url': baseUrl.value.trim()});
+    goHome();
+  };
+
+  baseUrl.addEventListener('keydown', e => {
+    if (e.keyCode!==13) {
+      return;
+    }
+
+    saveClicked(e);
+  });
+
   cancelButton.addEventListener('click', e => {
     e.preventDefault();
     goHome();
   });
 
-  saveButton.addEventListener('click', e => {
-    e.preventDefault();
-    chrome.storage.sync.set({'base_url': baseUrl.value.trim()});
-    goHome();
-  });
+  saveButton.addEventListener('click', e => saveClicked(e));
 
   chrome.storage.sync.get(['base_url'], r => {
     baseUrl.value = r.base_url===undefined ? '' :r.base_url;
